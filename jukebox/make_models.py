3dc8d6bae851c84788b59cfed1276d8ea3c9a7dd
picopicodragon <tastypudding@ny.tokai.or.jp>
2020-08-18 16:26:38 +0900

    kwargsテスト

--- jukebox/make_models.py
+++ jukebox/make_models.py
@@ -32,10 +32,6 @@ def load_checkpoint(path):
     if restore[:5] == 'gs://':
         gs_path = restore
         local_path = os.path.join(os.path.expanduser("~/.cache"), gs_path[5:])
-        # 手直し===============================================================================
-    elif restore[:16] == '/content/gdrive/':
-
-        #======================================================================================
         if dist.get_rank() % 8 == 0:
             print("Downloading from gce")
             if not os.path.exists(os.path.dirname(local_path)):
@@ -43,6 +39,9 @@ def load_checkpoint(path):
             if not os.path.exists(local_path):
                 download(gs_path, local_path)
         restore = local_path
+    # 手直し===============================================================================
+    #elif restore[:16] == '/content/gdrive/':
+    #======================================================================================
     dist.barrier()
     checkpoint = t.load(restore, map_location=t.device('cpu'))
     print("Restored from {}".format(restore))
